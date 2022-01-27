9112e2f8f33db201e1d88a0515ec1be1ba8fdeef
xoMichal <michalrusek198@gmail.com>
2022-01-27 21:30:39 +0100

    solving mistakes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Nav from '../components/Nav';
-import MainSection from '../components/MainSection';
-import Workers from '../components/Workers';
-import Offerts from '../components/Offerts';
-import Footer from '../components/Footer';
+import Nav from './components/Nav';
+import MainSection from './components/MainSection';
+import Workers from './components/Workers';
+import Offerts from './components/Offerts'
+import Footer from './components/Footer';
 
 {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" type='text/css' > <!--fonts link -->
 	
