79f0eb65eebb6c3b35413164794738914fc350df
xoMichal <michalrusek198@gmail.com>
2022-01-27 21:28:30 +0100

    solving errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Nav from '../components/Nav';
 import MainSection from '../components/MainSection';
 import Workers from '../components/Workers';
-import Offerts from '../components/Offerts'
+import Offerts from '../components/Offerts';
 import Footer from '../components/Footer';
 
 {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" type='text/css' > <!--fonts link -->
