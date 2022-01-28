15e40e57d604326e872cfa9132d9cc95ae0d2c07
xoMichal <michalrusek198@gmail.com>
2022-01-28 19:52:59 +0100

    links update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import './App.css';
 import Nav from './components/Nav';
 import MainSection from './components/MainSection';
 import Workers from './components/Workers';
-import Offerts from './components/Offerts'
+import Offerts from './components/Offerts';
 import Footer from './components/Footer';
-
-{/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" type='text/css' > <!--fonts link -->
-	
-  <script src="https://kit.fontawesome.com/d61d3b27e9.js" crossorigin="anonymous"></script> <!--icons link --> */}
-
 
 function App() {
   return (
